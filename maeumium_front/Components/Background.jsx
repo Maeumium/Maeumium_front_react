cc0a5b6118b8341842bf44412d04774725cbb37a
hooooooni <kheee2010@dgu.ac.kr>
2023-07-28 14:51:23 +0900

    인트로 페이지, 회원가입 페이지

---
+++ maeumium_front/Components/Background.jsx
@@ -0,0 +1,16 @@
+// const Background = styled.div `
+//     width: 100%;
+//     height: 100vh;
+//     background-image: url();
+//     background-repeat: no-repeat;
+//     background-position: center;
+//     background-size: cover;
+// `;
+
+// function Background ({children}) {
+//     return (
+//         <Background>
+//             {children}
+//         </Background>
+//     )
+// }
